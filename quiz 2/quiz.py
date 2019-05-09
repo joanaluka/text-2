368d6d084eb5eca0efdb788f5887438b4ab2335a
joanaluka <47768918+joanaluka@users.noreply.github.com>
2019-05-09 19:07:22 +0300

    Add files via upload

---
+++ quiz 2/quiz.py
@@ -0,0 +1,45 @@
+def list():
+    m=[100,110,120,130,140,150] 
+    for n in m:
+    	print(n%3)
+
+
+def sorted():
+  a=['apple','banana','mango']
+  b=['avocado','peach','orange']
+  c=['pineapple','lemon']
+  set=(a+b+c)
+  print(d)
+		
+def divisible_by_three():
+  number=range(1,n%1)
+  sum_number=sum(number)
+  print (sum_number)
+
+def function():
+    x=[[1,2],[3,4],[5,6]]
+    z=[1,2,3,4,5,6]
+    print(z)
+
+
+
+def smallest(): `                                                                                                             
+    a=[1,3,6,8,2,4,5.7]
+    print(min(1))
+
+def duplicates():
+	x=['a','b','a','e','d','b','c','e','f','g','h']
+	print('a','b','c','d')
+
+def square():
+	n=dict()
+    p= range(149,159)
+  for x in n:
+    n[x]= x*3
+  return (n)
+  
+def print_dict():
+    students = [{‘age’: 19, 'name': ‘Eunice’}, {‘age’: 21, 'name': 'Agnes'}, {‘age’: 18, 'name': 'Teresa'}, {age: 22, 'name': 'Asha'}]
+  for student in students:
+    print("Hello {}, you were born in the year {}.".format(student['name'],student['year']))
+
